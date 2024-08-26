166b2e33bf06592f6a1098da4a9ac46c9a1ea6b7
tranvanthinh <badagor999@gmail.com>
2024-08-26 23:51:53 +0700

    test up server

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -4,7 +4,7 @@ const initRoutes = (app) => {
   app.use("/api/v1/auth", authRouter);
 
   return app.use("/", (req, res) => {
-    res.send("server on ...");
+    res.send("server on 123");
   });
 };
 
